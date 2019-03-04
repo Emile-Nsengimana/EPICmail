c7b52c83ba7f6c04c35d91d771df00eb2f78317c
Emile Nsengimana <emilereas7@gmail.com>
2019-03-04 09:11:12 +0200

    User dashboard

--- js/mail.js
+++ js/mail.js
@@ -4,14 +4,23 @@ const clsGroup = document.getElementById('cls-group');
 const clsMessage = document.getElementById('cls-message');
 const createGroup = document.getElementById('create-group');
 const login = document.getElementById('btn-login');
-const btnCancelMsg = document.getElementsByClassName('btn-cancel1');
-const btnCancelGrp = document.getElementsByClassName('btn-cancel2');
+const btnCancelMsg = document.getElementById('btn-cancel1');
+const btnCancelGrp = document.getElementById('btn-cancel2');
 
 btnSend.addEventListener('click', () =>{
+    ran();
     document.getElementById('message').style.display = "none";
-    alert("Sent");
+    document.getElementById('undo').style.display = "block";
+    
 });
-
+function ran(){
+    setTimeout(function () {
+        document.getElementById('undo').style.display='none';
+    }, 7000);
+    document.getElementById('inbox').style.background = "rgba(0, 0, 0, 0.8)";
+document.getElementById('sent').style.background = "rgba(255, 255, 255, 1)";
+document.getElementById('draft').style.background = "rgba(255, 255, 255, 0.7)";
+}
 btnCompose.addEventListener('click', () => {
 document.getElementById('message').style.display = "block";
 document.getElementById('inbox').style.background = "rgba(0, 0, 0, 0.1)";
@@ -43,4 +52,8 @@ btnCancelMsg.addEventListener('click', () => {
 });
 btnCancelGrp.addEventListener('click', () => {
     document.getElementById('group').style.display = 'none';
+
+    document.getElementById('inbox').style.background = "rgba(0, 0, 0, 0.8)";
+document.getElementById('sent').style.background = "rgba(255, 255, 255, 1)";
+document.getElementById('draft').style.background = "rgba(255, 255, 255, 0.7)";
 });
