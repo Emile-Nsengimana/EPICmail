ede4f6b78340c8aa03b18ed4cc8ab31cca3c9ac8
Emile Nsengimana <emilereas7@gmail.com>
2019-03-06 11:31:21 +0200

    user api

---
+++ server/tests/userTest.js
@@ -0,0 +1,15 @@
+// import express from 'express';
+// import chai from 'chai';
+
+// const app = express();
+
+// describe('Get all users', () => {
+//   it('Should return object', () => {
+//     chai.request(app)
+//       .get('/api/v1/users')
+//       .end((err, res) => {
+//         chai.expect(res.statusCode).to.be.equal(200);
+//         chai.expect(res.body).to.be.a('object');
+//       });
+//   });
+// });
